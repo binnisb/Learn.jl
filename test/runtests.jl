c1844f557eaebadb15a2f8cdc834408262e18df1
Brynjar Smári Bjarnason <brynjar.bjarnason@kommuninvest.se>
2020-10-20 14:11:51 +0200

    testing all

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,5 +8,8 @@ using Test
     @test add(1.1, 3.0) == 4.1
     @test add(1+5im, 4-2im) == 5+3im
     @test add(Vector([1,2,3]), Vector([4,5,6])) == Vector([5,7,9])
+    @test add("asdf ", "fdsa") == "asdf fdsa"
+    @test ∑(1,2,3,4,5) == 15
+    @test ∑([1,2,3,4,5]) == 15
 
 end
