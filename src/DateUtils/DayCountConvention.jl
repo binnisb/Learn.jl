326e6bacc73a2196acdad4818cb824a454165dc8
Brynjar Smári Bjarnason <binni@binnisb.com>
2020-11-03 23:36:40 +0100

    Tests pass, refactor into days and yearfraction functions

--- src/DateUtils/DayCountConvention.jl
+++ src/DateUtils/DayCountConvention.jl
@@ -3,44 +3,56 @@ using Dates
 export AbstractDayCountConvention
 export Date
 export DayCount30_360, DayCount30A_360, DayCount30E_360, DayCount30E_360ISDA
+export DayCountAct, DayCountAct_360, DayCountAct_364
+export DayCountAct_365Fixed, DayCountAct_365Actual, DayCountAct_365Long, DayCountAct_365_25
+export DayCountAct_Act, DayCountAct_ActISDA
 
-export DayCount
+export days, yearfraction
 
 abstract type AbstractDayCountConvention end
 
 abstract type DayCount30_360 <: AbstractDayCountConvention end
-abstract type  DayCount30A_360 <: DayCount30_360 end
-abstract type  DayCount30E_360 <: DayCount30_360 end
-abstract type  DayCount30E_360ISDA <: DayCount30_360 end
+abstract type DayCountAct <: AbstractDayCountConvention end
+abstract type DayCountAct_Act <: AbstractDayCountConvention end
 
-struct DayCount{T <: AbstractDayCountConvention}
-    startDate::Date
-    endDate::Date
-    terminationDate::Date
-    days::Int
-    daysFraction::Float64
-end
+abstract type DayCount30A_360 <: DayCount30_360 end
+abstract type DayCount30E_360 <: DayCount30_360 end
+abstract type DayCount30E_360ISDA <: DayCount30_360 end
 
-function DayCount{T}(startDate::Date, endDate::Date,terminationDate::Date) where T <: DayCount30_360
-    dayCount = daysCount(startDate, endDate, terminationDate, T)
-    daysFraction = dayCount / 360.0
-    DayCount{T}(startDate, endDate, terminationDate, dayCount, daysFraction)
-end
+abstract type DayCountAct_360 <: DayCountAct end
+abstract type DayCountAct_364 <: DayCountAct end
+abstract type DayCountAct_365Fixed <: DayCountAct end
+abstract type DayCountAct_365_25 <: DayCountAct end
+abstract type DayCountAct_365Actual <: DayCountAct end
+abstract type DayCountAct_365Long <: DayCountAct end
 
-function daysCount(startDate::Date, endDate:: Date, terminationDate::Date, ::Type{T}) where T <: DayCount30A_360
-    d1 = day(startDate) ≤ 30 ? day(startDate) : 30
-    d2 = d1 ≠ 30 ? day(endDate) : min(day(endDate),30)
+abstract type DayCountAct_ActISDA <: DayCountAct_Act end
+
+
+@inline daysFormula(y1::Int, y2::Int, m1::Int, m2::Int, d1::Int, d2::Int)::Int = 360(y2-y1) + 30(m2-m1) + (d2-d1)
+function days(startDate::Date, endDate::Date, ::Type{T}) where T <: DayCount30A_360
+    d1 = min(day(startDate), 30)
+    de = day(endDate)
+    d2 = if d1 == 30
+        min(de, 30)
+    else
+        de
+    end
     daysFormula(year(startDate), year(endDate), month(startDate), month(endDate), d1, d2)
 end
-
-function daysCount(startDate::Date, endDate:: Date, terminationDate::Date, ::Type{T}) where T <: DayCount30E_360
-    d1 = day(startDate) ≤ 30 ? day(startDate) : 30
-    d2 = day(endDate) ≤ 30 ? day(endDate) : 30
-    daysFormula(year(startDate), year(endDate), month(startDate), month(endDate), d1, d2)
+function days(startDate::Date, endDate::Date, ::Type{T})::Int where T <: DayCount30E_360 
+    daysFormula(
+        year(startDate), year(endDate), month(startDate), month(endDate),
+        min(day(startDate), 30), min(day(endDate), 30)
+        )
 end
+function days(startDate::Date, endDate::Date, terminationDate::Date, ::Type{T})::Int where T <: DayCount30E_360ISDA
+    d1 = if startDate == Dates.lastdayofmonth(startDate)
+        30
+    else
+        day(startDate)
+    end
 
-function daysCount(startDate::Date, endDate:: Date, terminationDate::Date, ::Type{T}) where T <: DayCount30E_360ISDA
-    d1 = startDate == Dates.lastdayofmonth(startDate) ? 30 : day(startDate)
     d2 = if endDate == Dates.lastdayofmonth(endDate) && !(month(endDate) == 2 && endDate == terminationDate)
         30
     else
@@ -48,8 +60,27 @@ function daysCount(startDate::Date, endDate:: Date, terminationDate::Date, ::Typ
     end
     daysFormula(year(startDate), year(endDate), month(startDate), month(endDate), d1, d2)
 end
+yearfraction(startDate::Date, endDate::Date, ::Type{T}) where T <: DayCount30_360 = 
+    days(startDate, endDate, T) / 360.0
+yearfraction(startDate::Date, endDate::Date, terminationDate::Date, ::Type{T}) where T <: DayCount30_360 = 
+    days(startDate, endDate, terminationDate, T) / 360.0
 
-function daysFormula(y1::Int, y2::Int, m1::Int, m2::Int, d1::Int, d2::Int)::Int
-    360(y2-y1) + 30(m2-m1) + (d2-d1)
+days(startDate::Date, endDate::Date, ::Type{T}) where T <: DayCountAct = 
+    (endDate - startDate).value
+yearfraction(startDate::Date, endDate::Date, ::Type{T}) where T <: DayCountAct_360 = 
+    days(startDate, endDate, T) / 360.0
+yearfraction(startDate::Date, endDate::Date, ::Type{T}) where T <: DayCountAct_364 = 
+    days(startDate, endDate, T) / 364.0
+yearfraction(startDate::Date, endDate::Date, ::Type{T}) where T <: DayCountAct_365Fixed = 
+    days(startDate, endDate, T) / 365.0
+function yearfraction(startDate::Date, endDate::Date, ::Type{T}) where T <: DayCountAct_365Actual
+    d = days(startDate, endDate, T)
+    for y ∈ year(startDate):year(endDate)
+        if Dates.isleapyear(y) && startDate < Date(y,2,29) ≤ endDate
+            return d / 366.0
+        end
+    end
+    d / 365.0
 end
+
 end
--- test/testDayCounts.jl
+++ test/testDayCounts.jl
@@ -3,28 +3,34 @@ using Dates: Year
 
 toDate(dstr::String)::Date = Date(dstr, "mm/dd/yy") + Year(2000)
 
-@testset "DayCounts" begin
-    # Same dates for all test cases. Function for same processing of all 30/360 conventions
-    function test30360(daysResults, daysFracResults, dayCountType::Type{T}) where T <: DayCount30_360
-        # Test dates and values from https://www.isda.org/2008/12/22/30-360-day-count-conventions/ , https://www.isda.org/a/mIJEE/30-360-2006ISDADefs.xls
-        startDates = map(toDate, ["01/15/07", "01/15/07", "01/15/07", "09/30/07", "09/30/07", "09/30/07", "01/15/07", "01/31/07", "02/28/07", "08/31/06", "02/28/07", "02/14/07", "02/26/07", "02/29/08", "02/29/08", "02/29/08", "02/28/07", "10/31/07", "08/31/07", "02/29/08", "08/31/08", "02/28/09"])
-        endDates   = map(toDate, ["01/30/07", "02/15/07", "07/15/07", "03/31/08", "10/31/07", "09/30/08", "01/31/07", "02/28/07", "03/31/07", "02/28/07", "08/31/07", "02/28/07", "02/29/08", "02/28/09", "03/30/08", "03/31/08", "03/05/07", "11/28/07", "02/29/08", "08/31/08", "02/28/09", "08/31/09"])
-        terminationDate = toDate("02/28/09")
+# Same dates for all test cases
+# Test dates and values from https://www.isda.org/2008/12/22/30-360-day-count-conventions/ , https://www.isda.org/a/mIJEE/30-360-2006ISDADefs.xls
+startDates = map(toDate, ["01/15/07", "01/15/07", "01/15/07", "09/30/07", "09/30/07", "09/30/07", "01/15/07", "01/31/07", "02/28/07", "08/31/06", "02/28/07", "02/14/07", "02/26/07", "02/29/08", "02/29/08", "02/29/08", "02/28/07", "10/31/07", "08/31/07", "02/29/08", "08/31/08", "02/28/09"])
+endDates   = map(toDate, ["01/30/07", "02/15/07", "07/15/07", "03/31/08", "10/31/07", "09/30/08", "01/31/07", "02/28/07", "03/31/07", "02/28/07", "08/31/07", "02/28/07", "02/29/08", "02/28/09", "03/30/08", "03/31/08", "03/05/07", "11/28/07", "02/29/08", "08/31/08", "02/28/09", "08/31/09"])
+terminationDate = toDate("02/28/09")
 
-        for (sDate, eDate, days, dayFrac) in zip(startDates, endDates, daysResults, daysFracResults)
-            dc = DayCount{T}(sDate, eDate, terminationDate)
-            @test dc.daysFraction ≈ dayFrac atol=0.0001
-            @test dc.days == days
-        
-        end
+function test_helper(daysResults, daysFracResults, ::Type{T}) where T <: AbstractDayCountConvention
+    for (sDate, eDate, daysR, yearFracR) in zip(startDates, endDates, daysResults, daysFracResults)
+        @test days(sDate, eDate, T) == daysR
+        @test yearfraction(sDate, eDate, T) ≈ yearFracR atol=0.0001    
     end
+end
+
+function test_helper(daysResults, daysFracResults, tDate, ::Type{T}) where T <: AbstractDayCountConvention
+    for (sDate, eDate, daysR, yearFracR) in zip(startDates, endDates, daysResults, daysFracResults)
+        @test days(sDate, eDate, tDate, T) == daysR
+        @test yearfraction(sDate, eDate, tDate, T) ≈ yearFracR atol=0.0001
+    end
+end
+
+@testset "DayCounts" begin
 
     @testset "30/360 Bond Basis, 30A/360" begin
         # Test cases from https://www.isda.org/a/mIJEE/30-360-2006ISDADefs.xls
         testType = DayCount30A_360
         daysResults = [15,30,180,180,30,360,16,28,33,178,183,14,363,359,31,32,7,28,179,182,178,183]
         daysFracResults = [0.0417,0.0833,0.5000,0.5000,0.0833,1.0000,0.0444,0.0778,0.0917,0.4944,0.5083,0.0389,1.0083,0.9972,0.0861,0.0889,0.0194,0.0778,0.4972,0.5056,0.4944,0.5083]
-        test30360(daysResults, daysFracResults, testType)
+        test_helper(daysResults, daysFracResults, testType)
     end
 
     @testset "30E/360, 30/360 ICMA, 30/360 ISMA, 30S/360, Eurobond basis (ISDA 2006), Special German" begin
@@ -32,7 +38,7 @@ toDate(dstr::String)::Date = Date(dstr, "mm/dd/yy") + Year(2000)
         testType = DayCount30E_360
         daysResults = [15, 30, 180, 180, 30, 360, 15, 28, 32, 178, 182, 14, 363, 359, 31, 31, 7, 28, 179, 181, 178, 182]
         daysFracResults = [0.0417, 0.0833, 0.5000, 0.5000, 0.0833, 1.0000, 0.0417, 0.0778, 0.0889, 0.4944, 0.5056, 0.0389, 1.0083, 0.9972, 0.0861, 0.0861, 0.0194, 0.0778, 0.4972, 0.5028, 0.4944, 0.5056]
-        test30360(daysResults, daysFracResults, testType)
+        test_helper(daysResults, daysFracResults, testType)
     end
 
     @testset "30E/360ISDA, Eurobond basis (ISDA 2000), German" begin
@@ -40,6 +46,50 @@ toDate(dstr::String)::Date = Date(dstr, "mm/dd/yy") + Year(2000)
         testType = DayCount30E_360ISDA
         daysResults = [15, 30, 180, 180, 30, 360, 15, 30, 30, 180, 180, 16, 364, 358, 30, 30, 5, 28, 180, 180, 178, 180]
         daysFracResults = [0.0417, 0.0833, 0.5000, 0.5000, 0.0833, 1.0000, 0.0417, 0.0833, 0.0833, 0.5000, 0.5000, 0.0444, 1.0111, 0.9944, 0.0833, 0.0833, 0.0139, 0.0778, 0.5000, 0.5000, 0.4944, 0.5000]
-        test30360(daysResults, daysFracResults, testType)
+        test_helper(daysResults, daysFracResults, terminationDate, testType)
     end
+
+    @testset "Act/360, French" begin
+        # Test cases from https://www.isda.org/a/mIJEE/30-360-2006ISDADefs.xls
+        testType = DayCountAct_360
+        daysResults = [15, 31, 181, 183, 31, 366, 16, 28, 31, 181, 184, 14, 368, 365, 30, 31, 5, 28, 182, 184, 181, 184]
+        daysFracResults = [0.0417, 0.0861, 0.5028, 0.5083, 0.0861, 1.0167, 0.0444, 0.0778, 0.0861, 0.5028, 0.5111, 0.0389, 1.0222, 1.0139, 0.0833, 0.0861, 0.0139, 0.0778, 0.5056, 0.5111, 0.5028, 0.5111]
+        test_helper(daysResults, daysFracResults, testType)
+    end
+
+    @testset "Act/364" begin
+        testType = DayCountAct_364
+        daysResults = [15, 31, 181, 183, 31, 366, 16, 28, 31, 181, 184, 14, 368, 365, 30, 31, 5, 28, 182, 184, 181, 184]
+        daysFracResults = map(x-> x*360.0/364.0, [0.0417, 0.0861, 0.5028, 0.5083, 0.0861, 1.0167, 0.0444, 0.0778, 0.0861, 0.5028, 0.5111, 0.0389, 1.0222, 1.0139, 0.0833, 0.0861, 0.0139, 0.0778, 0.5056, 0.5111, 0.5028, 0.5111])
+        test_helper(daysResults, daysFracResults, testType)
+    end
+
+    @testset "Act/365Fixed" begin
+        testType = DayCountAct_365Fixed
+        daysResults = [15, 31, 181, 183, 31, 366, 16, 28, 31, 181, 184, 14, 368, 365, 30, 31, 5, 28, 182, 184, 181, 184]
+        daysFracResults = map(x-> x*360.0/365.0, [0.0417, 0.0861, 0.5028, 0.5083, 0.0861, 1.0167, 0.0444, 0.0778, 0.0861, 0.5028, 0.5111, 0.0389, 1.0222, 1.0139, 0.0833, 0.0861, 0.0139, 0.0778, 0.5056, 0.5111, 0.5028, 0.5111])
+        test_helper(daysResults, daysFracResults, testType)
+    end
+
+    @testset "Act/365Act" begin
+        testType = DayCountAct_365Actual
+        daysResults = [15, 31, 181, 183, 31, 366, 16, 28, 31, 181, 184, 14, 368, 365, 30, 31, 5, 28, 182, 184, 181, 184]
+        daysFracResults = map(x-> x*360.0/365.0, [0.0417, 0.0861, 0.5028, 0.5083, 0.0861, 1.0167, 0.0444, 0.0778, 0.0861, 0.5028, 0.5111, 0.0389, 1.0222, 1.0139, 0.0833, 0.0861, 0.0139, 0.0778, 0.5056, 0.5111, 0.5028, 0.5111])
+
+        for i in [4, 6, 13, 19]
+            daysFracResults[i] = daysFracResults[i]*365.0/366.0
+        end
+        test_helper(daysResults, daysFracResults, testType)
+    end
+
+    # @testset "Act/ActISDA" begin
+    #     testType = DayCountAct_Act_ISDA
+    #     daysResults = [15, 31, 181, 183, 31, 366, 16, 28, 31, 181, 184, 14, 368, 365, 30, 31, 5, 28, 182, 184, 181, 184]
+    #     daysFracResults = map(x-> x*360.0/365.0, [0.0417, 0.0861, 0.5028, 0.5083, 0.0861, 1.0167, 0.0444, 0.0778, 0.0861, 0.5028, 0.5111, 0.0389, 1.0222, 1.0139, 0.0833, 0.0861, 0.0139, 0.0778, 0.5056, 0.5111, 0.5028, 0.5111])
+
+    #     test_helper(daysResults, daysFracResults, testType)
+    # end
+
+    
+    
 end
